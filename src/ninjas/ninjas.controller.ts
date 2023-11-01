45cceb9911b92559dc2c60b13a4e5316c873d52e
AKHILESHPRATAP96 <114492226+AKHILESHPRATAP96@users.noreply.github.com>
2023-11-01 23:09:07 +0530

    Initial commit

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { create } from 'domain';
+import { CreateNinjaDto } from './dto/create-ninjas.dto';
+
+@Controller('ninjas')
+export class NinjasController {
+    @Get()
+    getninja():string{
+        return "This is the ninjas controller"
+    }
+
+    @Post("add")
+    addninja(@Body() createN:CreateNinjaDto):string{
+        return `adding new ninja ${createN.name}`
+    }
+    @Get(':id')
+    getninjsbyID(@Param('id') id:string){
+        return `geting ninja by id ${id}`;
+    }
+}
